5c063f006488531b5e4efbb19d69e89f07a39f9d
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-03 14:01:20 -0600

    pseduo-coding with instructor

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,6 @@
 var headBoard = document.getElementById("headboard");
 // #start page wrapper
 var startWrapper = document.getElementById("start-wrapper");
-// #start button
-var startBtn = document.getElementById("startBtn");
 // need id = quiz
 var quiz = document.getElementById("quiz-container");
 // need id = questions
@@ -26,21 +24,41 @@ var choice4 = document.getElementById("choice-4");
 // need id = result
 // need id = score list? or score board?
 
-// need array of questions
-var questions = [];
+
+
+// #start button
+var startBtn = document.getElementById("startBtn");
+// add eventListener to startGame function by clciking startBtn
+startBtn.addEventListener("click", startGame)
+
+// timer variables
+var countDown = document.getElementsByClassName("countdown"); // .countdown
+var timeLeft = 80; //availble time start
+var timeStart = 0; //time start
+// time textContent
+countdown.textContent = "Time: " + timeStart + "s";
+
+//timer function upon click startBtn
+function startTimer(){
+    let quizTime = setInterval(function(){
+        if (timeLeft > 0){
+            timeLeft = timeLeft -1;
+        }
+    })
+}
+
+
 
 // Pages : https://www.w3schools.com/jsref/prop_style_display.asp
 startWrapper.style.display = "block";
 // quizPage.style.display = 'none';
 // finalPage.style.display = 'none';
 
-// timer variables
-var countDown = document.getElementsByClassName("countdown"); // .countdown
-var timeLeft = 80; //availble time start
-var timeStart = 0; //time start
 
-// time textContent
-countdown.textContent = "Time: " + timeStart + "s";
+
+
+// need array of questions
+var questions = [];
 
 //need to define variables - WIP
 //need array of questions - waiting to recieve info from class
@@ -48,3 +66,29 @@ countdown.textContent = "Time: " + timeStart + "s";
 //need timer function, intervals - WIP
 //need prevent default,
 //need addEvenetLitsener
+
+
+
+
+
+//pseudo-code:
+
+//user is presented with 5 questions - arrray of objects
+    //format for questions???
+        //Object that contains {question: stirng, Choices: array, answer: string}
+        //Display questions - loop one by one 
+        //Append questions
+        //Append choices array
+
+// user selects an answer (button, radio, checkboxes) data-answer = ""
+    //click event is on the parent container (quiz or question wrapper)
+    //how to know which element was clicked(event.target)
+    //if the answer is correct display next question access array of questions object
+    //if answer is incorrect add 15 seconds to socre and display next question
+
+    //when all questions are answered display form to submit initial
+    //save form values socre and initials to loca storage
+
+//change to higihscore html
+    //read values form local storage
+    //append score values to page
